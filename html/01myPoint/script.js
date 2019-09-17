3a55741c12c0c8eafce07e54d24c80c604fcf11e
ihartog <26035@ma-web.nl>
2019-09-17 12:47:57 +0100

    hallo

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,21 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+var scale = 1;
+
+
+animate();
+
+function getRandom(max)
+{
+  return Math.floor(Math.random()*max);
+}
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  scale += 1;
+  let A = new Point(new Vector2d(getRandom(width),getRandom(height), getRandom(10)));
+  A.draw(context);
+}
