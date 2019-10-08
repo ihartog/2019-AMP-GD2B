8734fa749437ab800025e20842a099c20c768b4e
ihartog <26035@ma-web.nl>
2019-10-08 13:37:04 +0100

    perpendicular point

--- html/03draggablePoint/script.js
+++ html/03draggablePoint/script.js
@@ -7,11 +7,29 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(200, 200), 20);
+let grid = new Grid();
+
+let A = new Point(new Vector2d(200, 200), 20, true, "yellow");
+let B = new Point(new Vector2d(100, 200), 20, true, "blue");
+let C = new Point(new Vector2d(100, 100), 20, true, "green");
 
 function animation()
 {
   requestAnimationFrame(animation);
+
+  context.clearRect(0,0,width,height);
+  grid.draw(context);
+  context.beginPath();
+  context.fillStyle = "rgba(30,10,5,0.5)";
+  context.moveTo(A.position.dx,A.position.dy);
+  context.lineTo(B.position.dx,B.position.dy);
+  context.lineTo(C.position.dx,C.position.dy);
+  context.closePath();
+  context.stroke();
+  context.fill();
   A.draw(context);
+  B.draw(context);
+  C.draw(context);
+
 }
 animation();
