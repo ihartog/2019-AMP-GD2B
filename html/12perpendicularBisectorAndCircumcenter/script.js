99110018d7b4e0c94ff28edddba89f5c7c67d588
ihartog <26035@ma-web.nl>
2019-10-18 12:35:40 +0100

    opdracht 12 javascript

--- html/12perpendicularBisectorAndCircumcenter/script.js
+++ html/12perpendicularBisectorAndCircumcenter/script.js
@@ -10,6 +10,7 @@ canvas.height = height;
 let A = new Point(new Vector2d(200,200),20,true,"yellow");
 let B = new Point(new Vector2d(800,300),20,true,"blue");
 let C = new Point(new Vector2d(400,500),20,true,"red");
+let S = new Point(new Vector2d(0,0),10,false,"white");
 let mAB = new Point(new Vector2d(0,0),10,false,"white");
 let mBC = new Point(new Vector2d(0,0),10,false,"white");
 let mAC = new Point(new Vector2d(0,0),10,false,"white");
@@ -30,9 +31,26 @@ function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
 
+  S.position.dx = lab.intersection(lbc).x;
+  S.position.dy = lab.intersection(lbc).y;
+
+  let dSA = {};
+  dSA.dx = S.position.dx - A.position.dx;
+  dSA.dy = S.position.dy - A.position.dy;
+
+  dSA.r = Math.sqrt(dSA.dx * dSA.dx + dSA.dy * dSA.dy);
+
+  context.beginPath();
+  context.arc(S.position.dx,S.position.dy,dSA.r,0, Math.PI * 2);
+  context.closePath();
+  context.stroke();
+
+
+
   A.draw(context);
   B.draw(context);
   C.draw(context);
+  S.draw(context);
   ab.defineLineByTwoPoints(B,C);
   bc.defineLineByTwoPoints(B,A);
   ac.defineLineByTwoPoints(A,C);
@@ -50,10 +68,10 @@ function animate(){
  mAC.position.dx = (A.position.dx + C.position.dx) / 2;
  mAC.position.dy = (A.position.dy + C.position.dy) / 2;
 
- lab.slope = -1 / ab.slope;
+ lab.slope = -1 / bc.slope;
  lab.intercept = mAB.position.dy - lab.slope * mAB.position.dx;
 
- lbc.slope = -1 / bc.slope;
+ lbc.slope = -1 / ab.slope;
  lbc.intercept = mBC.position.dy - lbc.slope * mBC.position.dx;
 
  lac.slope = -1 / ac.slope;
