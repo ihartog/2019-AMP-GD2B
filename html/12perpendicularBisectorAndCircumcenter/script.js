275896020efd34c1c07837bd11779bc8ba0dd3ef
ihartog <26035@ma-web.nl>
2019-10-18 12:18:25 +0100

    part of assignment 12

--- html/12perpendicularBisectorAndCircumcenter/script.js
+++ html/12perpendicularBisectorAndCircumcenter/script.js
@@ -1,4 +1,3 @@
-
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
@@ -11,59 +10,61 @@ canvas.height = height;
 let A = new Point(new Vector2d(200,200),20,true,"yellow");
 let B = new Point(new Vector2d(800,300),20,true,"blue");
 let C = new Point(new Vector2d(400,500),20,true,"red");
-let S = new Point(new Vector2d(0,0),10,false,"white");
-let mAB = new Point(new Vector2d(0,0), 10, false, "white");
-let mBC = new Point(new Vector2d(0,0), 10, false, "white");
-let mCA = new Point(new Vector2d(0,0), 10, false, "white");
+let mAB = new Point(new Vector2d(0,0),10,false,"white");
+let mBC = new Point(new Vector2d(0,0),10,false,"white");
+let mAC = new Point(new Vector2d(0,0),10,false,"white");
 
 A.drag();
 B.drag();
 C.drag();
 
-let lab = new LinearFunction(1,1);
-let lbc = new LinearFunction(1,1);
-let lca = new LinearFunction(1,1);
 let ab = new LinearFunction(0,0);
 let bc = new LinearFunction(0,0);
-let ca = new LinearFunction(0,0);
+let ac = new LinearFunction(0,0);
+let lab = new LinearFunction(0,0);
+let lbc = new LinearFunction(0,0);
+let lac = new LinearFunction(0,0);
 
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
 
-//  m.draw(context);
-
   A.draw(context);
   B.draw(context);
   C.draw(context);
   ab.defineLineByTwoPoints(B,C);
   bc.defineLineByTwoPoints(B,A);
-  ca.defineLineByTwoPoints(A,C);
+  ac.defineLineByTwoPoints(A,C);
 
  ab.draw(context);
-
-lbc.slope = -1 / bc.slope;
-lbc.intercept = C.position.dy - lbc.slope * C.position.dx;
-
-lab.slope = -1 / ab.slope;
-lab.intercept = A.position.dy - lab.slope * A.position.dx;
-
  bc.draw(context);
- ca.draw(context);
+ ac.draw(context);
 
-lca.slope = -1 / ca.slope;
-lca.intercept = B.position.dy - lca.slope * B.position.dx;
+ mAB.position.dx = (A.position.dx + B.position.dx) / 2;
+ mAB.position.dy = (A.position.dy + B.position.dy) / 2;
 
- lca.draw(context, "red");
- lbc.draw(context, "blue");
- lab.draw(context, "purple");
+ mBC.position.dx = (B.position.dx + C.position.dx) / 2;
+ mBC.position.dy = (B.position.dy + C.position.dy) / 2;
 
-console.log(lab.intersection(lbc));
- S.position.dx = lab.intersection(lbc).x;
-  S.position.dy = lab.intersection(lbc).y;
+ mAC.position.dx = (A.position.dx + C.position.dx) / 2;
+ mAC.position.dy = (A.position.dy + C.position.dy) / 2;
 
-  S.draw(context);
+ lab.slope = -1 / ab.slope;
+ lab.intercept = mAB.position.dy - lab.slope * mAB.position.dx;
+
+ lbc.slope = -1 / bc.slope;
+ lbc.intercept = mBC.position.dy - lbc.slope * mBC.position.dx;
+
+ lac.slope = -1 / ac.slope;
+ lac.intercept = mAC.position.dy - lac.slope * mAC.position.dx;
+
+ mAB.draw(context);
+ mBC.draw(context);
+ mAC.draw(context);
+ lab.draw(context,"green");
+ lbc.draw(context,"blue");
+ lac.draw(context,"red");
 
 }
 
