1c77c5fb9af1e903027003c714456b07e1aed81e
ihartog <26035@ma-web.nl>
2019-10-29 15:06:25 +0100

    pijl die naar de cursor wijst doormiddel van javascript

--- html/11mousePointer/script.js
+++ html/11mousePointer/script.js
@@ -7,14 +7,25 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+let mouse = {};
+
 let arrow = new Arrow(new Vector2d(canvas.width/2, canvas.height/2),0);
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0, canvas.width, canvas.height);
 
-  arrow.angle += 0.01;
+  let mx = mouse.x - arrow.pos.dx;
+  let my = mouse.y - arrow.pos.dy;
+
+  arrow.angle = Math.atan2(my, mx);
   arrow.draw(context);
 }
 
 animate();
+
+addEventListener("mousemove", (evt)=>
+{
+  mouse.x = evt.clientX;
+  mouse.y = evt.clientY;
+});
