33b351ea8aada2e1f5a48865947c261fec010c30
ihartog <26035@ma-web.nl>
2020-03-25 13:38:54 +0100

    updates

--- html/20PullUpToMyBumper/script.js
+++ html/20PullUpToMyBumper/script.js
@@ -10,14 +10,18 @@ canvas.height = height;
 let ball, bumper;
 
 bumper = new Point(new Vector2d(width/2, height/2),200,false, "yellow");
-ball = new DPoint(new Vector2d(100,100), new Vector2d(1,1),new Vector2d(0,0) ,20,true, "red");
-
+ball = new DPoint(new Vector2d(10,10), new Vector2d(10,10),new Vector2d(0,0) ,20,true, "red");
+ball.radius = new Vector2d(1,1);
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
   ball.Update();
+  ball.radius.dx = bumper.position.dx - ball.pos.dx;
+  ball.radius.dy = bumper.position.dy - ball.pos.dy;
+
   ball.vel.draw(context,ball.pos,20,"red");
+  ball.radius.draw(context,ball.pos,1,"red");
   bumper.draw(context);
   ball.draw(context);
 }
